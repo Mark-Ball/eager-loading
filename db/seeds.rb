037ff3416a2ab706ff780236d7ed6892e4ea9874
Mark-Ball <markball111@gmail.com>
2019-11-28 16:37:07 +1100

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..10
+    User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email
+    )
+    puts "Created user #{i} of 10"
+end
+
+numUsers = User.all.length
+for i in 1..numUsers
+    User.find(i).create_address(
+        street: Faker::Address.street_address,
+        city: Faker::Address.city
+    )
+    puts "Created address #{i} of #{numUsers}"
+end
