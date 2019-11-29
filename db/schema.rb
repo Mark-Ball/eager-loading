2fdbc872e052900ee2a3d007380f793a438fac99
Mark-Ball <markball111@gmail.com>
2019-11-30 09:54:12 +1100

    created listings table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_051252) do
+ActiveRecord::Schema.define(version: 2019_11_29_224741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2019_11_28_051252) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -30,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_11_28_051252) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "listings", "users"
 end
