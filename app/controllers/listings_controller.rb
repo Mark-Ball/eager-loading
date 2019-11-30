aa870b0644074cbc385550c2574f078603d93d95
Mark-Ball <markball111@gmail.com>
2019-11-30 15:36:57 +1100

    added eager loading for nested models

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,8 @@ class ListingsController < ApplicationController
     def index
         @listings = Listing.all
     end
+
+    def eager
+        @listings = Listing.includes(:user => :address, :image_attachment => :blob).all
+    end
 end
