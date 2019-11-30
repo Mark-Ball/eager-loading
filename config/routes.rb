d0a7fb709f12e783abe4e4b11af087e7449fe266
Mark-Ball <markball111@gmail.com>
2019-11-30 11:12:53 +1100

    added listings controller, routes and views for lazy loading page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/users", to: "users#index", as: "users"
   get "/users/eager", to: "users#eager", as: "eager_users"
+  get "/listings", to: "listings#index", as: "listings"
+  get "/listings/eager", to: "listings#eager", as: "eager_listings"
 end
