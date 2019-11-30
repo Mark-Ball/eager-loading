4d268d22322e929a26eaddb1631b4a9a4cae3a09
Mark-Ball <markball111@gmail.com>
2019-11-30 14:56:45 +1100

    completed listing page with lazy loading

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# for i in 1..10
-#     User.create(
-#         name: Faker::Name.name,
-#         email: Faker::Internet.email
-#     )
-#     puts "Created user #{i} of 10"
-# end
+for i in 1..10
+    User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email
+    )
+    puts "Created user #{i} of 10"
+end
 
-# numUsers = User.all.length
-# for i in 1..numUsers
-#     User.find(i).create_address(
-#         street: Faker::Address.street_address,
-#         city: Faker::Address.city
-#     )
-#     puts "Created address #{i} of #{numUsers}"
-# end
+numUsers = User.all.length
+for i in 1..numUsers
+    User.find(i).create_address(
+        street: Faker::Address.street_address,
+        city: Faker::Address.city
+    )
+    puts "Created address #{i} of #{numUsers}"
+end
 
-# for i in 1..20
-#     Listing.create(
-#         name: Faker::Book.title,
-#         price: rand(1..1000),
-#         user_id: rand(1..10)
-#     )
-#     puts "Created book #{i} of 20"
-# end
+for i in 1..20
+    Listing.create(
+        name: Faker::Book.title,
+        price: rand(1..1000),
+        user_id: rand(1..10)
+    )
+    puts "Created book #{i} of 20"
+end
 
 for i in 1..20
     img = Down.download(Faker::LoremPixel.image(size: "200x200", is_gray: false, category: 'nature'))
